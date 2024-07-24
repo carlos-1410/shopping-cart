6252fa4d7a154f4cf32c78135117e9a2302cb80d
Łukasz Kerl <kerlak11@gmail.com>
2024-07-24 21:15:27 +0200

    Remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@
 Rails.application.routes.draw do
   resources :pricing_rules, except: %i(new)
   get "pricing_rules/:product_id/new", to: "pricing_rules#new", as: :new_pricing_rule
-  post "pricing_rules/activate", to: "pricing_rules#activate", as: :activate_pricing_rule
-  post "pricing_rules/deactivate", to: "pricing_rules#deactivate", as: :deactivate_pricing_rule
+
   resources :products
 
   root "products#index"
